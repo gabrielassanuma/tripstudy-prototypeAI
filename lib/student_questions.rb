7fb902f2af9315505cb17a9c01b5f58883533c1b
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-05-02 16:36:42 +0100

    added more logic to integration.rb

--- lib/student_questions.rb
+++ lib/student_questions.rb
@@ -1,11 +1,6 @@
-require "openai"
-require 'dotenv'
-require_relative 'students_questions.rb'
-Dotenv.load
-
-answer = []
-students_questions = [
+STUDENT_QUESTIONS = [
   "Qual é o visto de estudante necessário para estudar na Austrália?",
+  "Como faço para solicitar um visto de estudante australiano?",
   "Qual é o processo de aplicação para um visto de estudante australiano?",
   "Quais são os requisitos para a obtenção de um visto de estudante australiano?",
   "Quais documentos são necessários para solicitar um visto de estudante australiano?",
@@ -15,33 +10,18 @@ students_questions = [
   "Posso renovar meu visto de estudante australiano?",
   "Quais são as taxas associadas ao visto de estudante australiano?",
   "Posso trazer minha família para a Austrália comigo com um visto de estudante?",
+  "Quais são os requisitos financeiros para um visto de estudante australiano?",
   "Preciso de um seguro de saúde para solicitar um visto de estudante australiano?",
   "Como faço para encontrar acomodação enquanto estudo na Austrália?",
   "É necessário fazer um exame médico para solicitar um visto de estudante australiano?",
   "Quais são as restrições de viagem com um visto de estudante australiano?",
   "Posso viajar para fora da Austrália com um visto de estudante australiano?",
+  "O que acontece se eu não seguir as regras do meu visto de estudante australiano?",
+  "Quanto tempo posso permanecer na Austrália com um visto de estudante australiano?",
   "Posso mudar de curso ou instituição enquanto estudo com um visto de estudante australiano?",
   "Preciso de um agente de imigração para me ajudar com o meu visto de estudante australiano?",
   "O que acontece se meu visto de estudante australiano for negado?",
   "Como faço para renovar meu visto de estudante australiano?",
+  "Qual é a diferença entre um visto de estudante e um visto de trabalho na Austrália?",
+  "Posso aplicar para um visto de estudante australiano se eu já tiver sido deportado da Austrália antes?"
 ]
-
-openAI_key = ENV['OPENAI_KEY']
-client = OpenAI::Client.new(access_token: openAI_key)
-students_questions.each do |question|
-  response = client.chat(
-      parameters: {
-          model: "gpt-3.5-turbo", 
-          messages: [{ role: "user", content: question}],
-          temperature: 0.2,
-          max_tokens: 200
-      })
-  answer << response.dig("choices", 0, "message", "content")
-  sleep(5)
-end
-
-students_questions.each_with_index do |question, index|
-  puts "Question #{index + 1}: #{question.strip}"
-  p "      Answer: #{answer[index]}"
-  puts ""
-end
