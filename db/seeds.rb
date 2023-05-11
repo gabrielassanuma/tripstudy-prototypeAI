29a0014be601179f7645d984871aa75f94bf1013
gabriel assanuma <gabriel.assanuma@gmail.com>
2023-05-11 14:51:41 +0100

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,20 @@ AU_STATES = [
   'Tasmania', 'Victoria', 'Western Australia', 'Australian Capital Territory', 'Northern Territory'
 ]
 
+PRICES = (10000..60000).step(10000).to_a
+
 # Helper method to generate a random duration between 2 and 5 years
 def generate_duration
   "#{rand(2..5)} years"
 end
 
-# Helper method to generate a random price between $10,000 and $32,000 AUD
-def generate_price
-  rand(10000..32000)
-end
 
 # Create 10 sample courses with random data
 30.times do
   Course.create!(
     name: Faker::Educator.course_name,
-    description: Faker::Lorem.paragraph,
-    price: generate_price,
+    description: "Course description",
+    price: PRICES.sample,
     duration: generate_duration,
     location: AU_STATES.sample
   )
